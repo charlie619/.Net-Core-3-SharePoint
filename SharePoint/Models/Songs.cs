d5ced4bc1a6ec1a3589691ff4b05e898ba5ef5e3
Gurpreet Singh <Gurpreet.charlie@gmail.com>
2021-08-15 21:12:16 +0100

    Applied server & client Validation

--- SharePoint/Models/Songs.cs
+++ SharePoint/Models/Songs.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,14 @@ namespace SharePoint.Models
     public class Songs
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
         [DisplayName("Release Date (mm.yyyy)")]
+        [Required]
+        
+        [RegularExpression(@"^(\d{2}|\d{1})*(\.)?([0-9]{4})?$", ErrorMessage = "Numbers only & should be in (MM.YYYY) Format")]
         public double ReleaseDate { get; set; }
     }
 }
